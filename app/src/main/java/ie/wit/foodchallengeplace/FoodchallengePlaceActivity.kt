a803a7388328507e0401a0ccf1570c3076e85ea5
Caroline <76437312+Colarein@users.noreply.github.com>
2022-09-22 19:29:25 +0100

    Updated app to include Food challenge place and "Feeling Hungry"

--- app/src/main/java/ie/wit/foodchallengeplace/FoodchallengePlaceActivity.kt
+++ app/src/main/java/ie/wit/foodchallengeplace/FoodchallengePlaceActivity.kt
@@ -3,9 +3,9 @@ package ie.wit.foodchallengeplace
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class FoodchallengePlaceActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_foodchallengesplace)
     }
 }
