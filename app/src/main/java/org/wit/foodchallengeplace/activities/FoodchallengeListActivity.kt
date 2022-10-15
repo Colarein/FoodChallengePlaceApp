2b5b4540f5d99936832f11d468156a2b178f203c
Caroline <76437312+Colarein@users.noreply.github.com>
2022-10-15 14:47:30 +0100

    Lab 06 Exercise 4 Refactored Food Challenge place to exclusively use the recommended ActivityResults API and removed the now deprecated calls to startActivityForResult() and onActivityResult()

--- app/src/main/java/org/wit/foodchallengeplace/activities/FoodchallengeListActivity.kt
+++ app/src/main/java/org/wit/foodchallengeplace/activities/FoodchallengeListActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import org.wit.foodchallengeplace.R
@@ -16,6 +18,7 @@ import org.wit.foodchallengeplace.models.FoodchallengePlaceModel
 class FoodchallengeListActivity : AppCompatActivity(), FoodchallengePlaceListener {
     lateinit var app: MainApp
     private lateinit var binding: ActivityFoodchallengeListBinding
+    private lateinit var refreshIntentLauncher : ActivityResultLauncher<Intent>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +32,8 @@ class FoodchallengeListActivity : AppCompatActivity(), FoodchallengePlaceListene
         val layoutManager = LinearLayoutManager(this)
         binding.recyclerView.layoutManager = layoutManager
         binding.recyclerView.adapter = FoodchallengePlaceAdapter(app.foodchallengeplaces.findAll(), this)
+
+        registerRefreshCallback()
     }
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_main, menu)
@@ -39,7 +44,7 @@ class FoodchallengeListActivity : AppCompatActivity(), FoodchallengePlaceListene
         when (item.itemId) {
             R.id.item_add -> {
                 val launcherIntent = Intent(this, FoodchallengePlaceActivity::class.java)
-                startActivityForResult(launcherIntent,0)
+                refreshIntentLauncher.launch(launcherIntent)
             }
         }
         return super.onOptionsItemSelected(item)
@@ -48,11 +53,12 @@ class FoodchallengeListActivity : AppCompatActivity(), FoodchallengePlaceListene
     override fun onFoodchallengeplaceClick(foodchallengeplace: FoodchallengePlaceModel) {
         val launcherIntent = Intent(this, FoodchallengePlaceActivity::class.java)
         launcherIntent.putExtra("foodchallengeplace_edit", foodchallengeplace)
-        startActivityForResult(launcherIntent,0)
+        refreshIntentLauncher.launch(launcherIntent)
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        binding.recyclerView.adapter?.notifyDataSetChanged()
-        super.onActivityResult(requestCode, resultCode, data)
+    private fun registerRefreshCallback() {
+        refreshIntentLauncher =
+            registerForActivityResult(ActivityResultContracts.StartActivityForResult())
+            { binding.recyclerView.adapter?.notifyDataSetChanged() }
     }
 }
