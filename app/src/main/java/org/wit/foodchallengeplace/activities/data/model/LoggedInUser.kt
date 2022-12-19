829cbc48c0398f7831b703914fe0e86301403fc9
Caroline <76437312+Colarein@users.noreply.github.com>
2022-12-19 21:28:44 +0000

    Deleted unused activity and layout files

--- app/src/main/java/org/wit/foodchallengeplace/activities/data/model/LoggedInUser.kt
+++
@@ -1,9 +0,0 @@
-package org.wit.foodchallengeplace.activities.data.model
-
-/**
- * Data class that captures user information for logged in users retrieved from LoginRepository
- */
-data class LoggedInUser(
-    val userId: String,
-    val displayName: String
-)
